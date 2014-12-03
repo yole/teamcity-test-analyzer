ce5de95bfd3c3445c14757f48db9be342ca7d855
Dmitry Jemerov <yole@jetbrains.com>
2014-12-03 18:04:38 +0100

    work in progress

--- src/main/kotlin/tcapi.kt
+++ src/main/kotlin/tcapi.kt
@@ -5,21 +5,39 @@ import retrofit.http.Query
 import retrofit.http.Headers
 import java.util.ArrayList
 import retrofit.client.Response
+import retrofit.http.Path
 
 class BuildList() {
     var build: List<Build> = ArrayList()
 }
 
-class Build() {
-    var id: String = ""
-    var href: String = ""
+class Build(val id: String, val href: String)
+
+class TestOccurrenceList() {
+    var testOccurrence: List<TestOccurrence> = ArrayList()
 }
+
+class TestOccurrence(val id: String, val name: String, val duration: Int)
+
+class PropertyList() {
+    var property: List<Property> = ArrayList()
+}
+
+class Property(val name: String, val value: Int)
 
 trait TeamCityService {
     GET("/guestAuth/app/rest/builds")
     Headers("Accept: application/json")
     fun listBuilds(Query("locator") locator: String): BuildList
 
-    GET("/downloadBuildLog.html")
+    GET("/guestAuth/app/rest/testOccurrences")
+    Headers("Accept: application/json")
+    fun listTestOccurrences(Query("locator") locator: String): TestOccurrenceList
+
+    GET("/guestAuth/app/rest/builds/{locator}/statistics")
+    Headers("Accept: application/json")
+    fun listStatistics(Path("locator") locator: String): PropertyList
+
+    GET("/guestAuth/downloadBuildLog.html")
     fun downloadBuildLog(Query("buildId") buildId: String): Response
 }
--- src/main/kotlin/tcta.kt
+++ src/main/kotlin/tcta.kt
@@ -1,6 +1,11 @@
 package org.jetbrains.teamCityTestAnalyzer
 
 import retrofit.RestAdapter
+import retrofit.RetrofitError
+import java.util.HashMap
+import java.text.SimpleDateFormat
+import org.joda.time.Duration
+import org.joda.time.format.PeriodFormat
 
 class Analyzer(serverAddress: String, val buildTypeId: String) {
     val teamcity = RestAdapter.Builder()
@@ -18,7 +23,40 @@ class Analyzer(serverAddress: String, val buildTypeId: String) {
     }
 
     fun processBuild(build: Build) {
-        downloadBuildLog(build.id)
+        val testOccurrences = downloadTestOccurrences(build.id).sortDescendingBy { it.duration }
+        val statistics = downloadStatistics(build.id)
+        val totalTestExecutionTime = testOccurrences.fold(0, { (time, test) -> time + test.duration })
+        val testCount = testOccurrences.size
+        val totalBuildTime = statistics["BuildDuration"]
+        if (totalBuildTime == null) {
+            println("Missing total build time")
+            return
+        }
+        val compilationTime = statistics["Compilation time, ms"]
+        println("Total build time ${formatTime(totalBuildTime)}")
+        if (compilationTime != null) {
+            val compilationTimePercent = compilationTime.toDouble() / totalBuildTime.toDouble() * 100;
+            println("Compilation time ${formatTime(compilationTime)} (${compilationTimePercent} %)")
+        }
+        println("Test execution time ${formatTime(totalTestExecutionTime)}")
+    }
+
+    fun formatTime(timeInMS: Int): String {
+        val duration = Duration.millis(timeInMS.toLong())
+        return "${timeInMS} ms (" + PeriodFormat.getDefault().print(duration.toPeriod()) + ")"
+    }
+
+    fun downloadTestOccurrences(id: String): List<TestOccurrence> {
+        val locator = "count:30000,build:(id:${id})"
+        return teamcity.listTestOccurrences(locator).testOccurrence
+    }
+
+    fun downloadStatistics(id: String): Map<String, Int> {
+        val locator = "id:${id}"
+        val statistics = teamcity.listStatistics(locator).property
+        val result = HashMap<String, Int>()
+        statistics.forEach { result.put(it.name, it.value) }
+        return result
     }
 
     fun downloadBuildLog(id: String) {
@@ -28,7 +66,7 @@ class Analyzer(serverAddress: String, val buildTypeId: String) {
 }
 
 fun main(args: Array<String>) {
-    if (args.size() != 2) {
+    if (args.size != 2) {
         println("Usage: teamCityTestAnalyzer <server> <build type ID>")
         return
     }
